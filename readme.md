87e2b90cbf51207fd05300a5146adbfe4ee9e272
Kaleb <kaleblub@gmail.com>
2021-08-28 00:03:56 +0300

    Getting started with my custom pygame template

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Space Invaders With Pygame
+
+This is my project of building the game Space Invaders with Pygame.
+
+## Requirements
+This code requires the [pygame](https://www.pygame.org/docs/) Library which can be installed with
+```bash
+pip install pygame
+```
+## Installation
+For installation, use
+```bash
+git clone https://github.com/kaleblub/pygame-space-invaders.git
+```
+## Usage
+To run the game, mv into the pygame-space-invaders directory, and use
+```bash
+python3 space_invaders.py
+```
+
+## Other
+To follow my journey of building this and other projects, find them [here!](https://kaleblub.github.io/blog)
---
+++ space_invaders.py
@@ -0,0 +1,78 @@
+import pygame
+# ----------- Game Initialization -------------------
+pygame.init()
+
+displayWidth, displayHeight = 700, 800
+
+gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
+pygame.display.set_caption('Space Invaders')
+clock = pygame.time.Clock()
+
+# ----------- Constants ---------------
+FPS = 60
+backgroundColor = "black"
+
+
+# ----------- Main Game Function ---------------
+def runGame():
+	# Game Variables
+	gameRunning = True
+	gameOver = False
+
+# ----------- Start Of Game Loop ---------------
+	while gameRunning:
+		gameDisplay.fill(backgroundColor)
+
+# ----------- Game Over Menu -------------------
+#	Use this loop as a template for any other screens you want to add.
+		while gameOver == True:
+			pygame.display.update()
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					gameOver = False
+					gameRunning = False
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						gameRunning = False
+						gameOver = False
+					if event.key == pygame.K_c:
+						runGame()
+
+# ----------- Gameplay Handling -------------------
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameRunning = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				pass
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT or event.key == ord('a'):
+					print('left')
+				if event.key == pygame.K_RIGHT or event.key == ord('d'):
+					print('right')
+				if event.key == pygame.K_UP or event.key == ord('w'):
+					print('jump')
+				if event.key == pygame.K_DOWN or event.key == ord('s'):
+					print('down')
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == ord('a'):
+					print('left stop')
+				if event.key == pygame.K_RIGHT or event.key == ord('d'):
+					print('right stop')
+				if event.key == pygame.K_DOWN or event.key == ord('s'):
+					print('down stop')
+				if event.key == ord('q'):
+					pygame.quit()
+					sys.exit()
+					gameRunning = False
+
+#  ----------- Game Code -------------------
+		# Draw
+
+		# Update
+		pygame.display.update()
+		clock.tick(FPS)
+
+		
+if __name__ == "__main__":
+	runGame()
